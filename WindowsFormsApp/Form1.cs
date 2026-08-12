e6b4b355205dbaccf88926e590ae7c1520b0f9eb
PaballoMolebo <st10499390@rcconnect.edu.za>
2026-08-12 17:00:59 +0200

    Add files via upload

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,3 +1,4 @@
+using System.Media;
 namespace WindowsFormsApp
 {
     public partial class Form1 : Form
@@ -9,12 +10,27 @@ namespace WindowsFormsApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            PlayGreeting();
+            DisplayLogo();
         }
 
-        private void button2_Click(object sender, EventArgs e) 
+        private void PlayGreeting()
         {
-        
+            SoundPlayer player = new SoundPlayer("greeting.wav");
+            player.Play();
         }
+
+        private void DisplayLogo()
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Users\\User\\Pictures\\image.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
     }
 }
